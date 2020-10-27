dbb1f9466e9363c3e3e9bf37e121e1ba9c7675e3
GeethOnion <geethmaka@gmail.com>
2020-10-27 14:51:42 +0530

    database updated

--- onlinemarketstoreDatabase.sql
+++ onlinemarketstoreDatabase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2020 at 04:42 AM
+-- Generation Time: Oct 27, 2020 at 10:20 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -38,7 +38,8 @@ CREATE TABLE `assigneddelivery` (
 INSERT INTO `assigneddelivery` (`DP_ID`, `OID`) VALUES
 (6, 19),
 (6, 20),
-(6, 21);
+(6, 21),
+(6, 22);
 
 -- --------------------------------------------------------
 
@@ -53,11 +54,11 @@ CREATE TABLE `customer` (
   `NIC` varchar(12) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `Phonenumber` int(11) NOT NULL,
+  `Phonenumber` varchar(11) NOT NULL,
   `Address` varchar(30) NOT NULL,
   `City` varchar(20) NOT NULL,
   `Cardtype` varchar(10) NOT NULL,
-  `Cardno` int(16) DEFAULT NULL,
+  `Cardno` varchar(16) DEFAULT NULL,
   `Expirydate` date NOT NULL,
   `CVV` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -67,7 +68,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`CID`, `firstname`, `lastname`, `NIC`, `Email`, `Password`, `Phonenumber`, `Address`, `City`, `Cardtype`, `Cardno`, `Expirydate`, `CVV`) VALUES
-(5, 'Ajith', 'Danapala', '691264785v', 'customer@gmail.com', 'Password$123', 123456789, 'Pilimathalawa', 'Kandy', 'visa', 2147483647, '2020-10-01', 111);
+(5, 'Ajith', 'Danapala', '691264785v', 'customer@gmail.com', 'Password$123', '123456789', 'Pilimathalawa', 'Kandy', 'visa', '2147483647', '2020-10-01', 111),
+(7, 'Nipun', 'Senarath', '200017902852', 'nipun@gmail.com', 'Nipun2000', '94773969049', 'No.70 Weligalla road', 'Mawanella', 'visa', '1234123412341234', '2020-12-01', 123);
 
 -- --------------------------------------------------------
 
@@ -91,7 +93,8 @@ CREATE TABLE `deliveryperson` (
 --
 
 INSERT INTO `deliveryperson` (`DP_ID`, `User_name`, `NIC`, `Email`, `Phone_number`, `Password`, `Vehical_No`, `Location`) VALUES
-(6, 'Sunil Samarakoon', '697598464v', 'deliveryp@buyme.com', 910123456, 'Password#person', 'JJ-2222', 'Kandy');
+(6, 'Sunil Samarakoon', '697598464v', 'deliveryp@buyme.com', 910123456, 'Password#person', 'JJ-2222', 'Kandy'),
+(7, 'Sanath Amarakoon', '697598864v', 'deliveryp1@buyme.com', 910123456, 'Password#person', 'KJ-7895', 'Mawanella');
 
 -- --------------------------------------------------------
 
@@ -105,6 +108,13 @@ CREATE TABLE `feedback` (
   `Description` varchar(1000) NOT NULL,
   `CID` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`FID`, `Topic`, `Description`, `CID`) VALUES
+(2, 'Seller shipping ', 'The shipping is too late. I didnt receive the item still.', 7);
 
 -- --------------------------------------------------------
 
@@ -131,7 +141,7 @@ INSERT INTO `item` (`Item_number`, `SID`, `Item_Name`, `Description`, `Quantity`
 (5, 6, 'Wireless Mouse', 'Logitech Wireless Mouse', 100, 500, 0, 'Electronics'),
 (6, 6, 'Micro SD card 64GB', 'Micro SD card 64GB', 150, 1000, 0, 'Electronics'),
 (11, 6, 'Micro SD card 32GB', 'Micro SD card 32GB', 150, 1000, 0, 'Electronics'),
-(12, 7, 'Gold chain', '24K gold chain', 5, 0, 0, 'Jewelry & Watches');
+(12, 7, 'Gold chain', '24K gold chain', 5, 0, 0, 'JewelryWatches');
 
 -- --------------------------------------------------------
 
@@ -153,7 +163,9 @@ CREATE TABLE `orderitem` (
 INSERT INTO `orderitem` (`id`, `Item_number`, `OID`, `Quantity`) VALUES
 (9, 4, 19, '10'),
 (14, 4, 20, '1'),
-(15, 5, 21, '2');
+(15, 5, 21, '2'),
+(16, 5, 22, '2'),
+(17, 5, 23, '5');
 
 -- --------------------------------------------------------
 
@@ -175,7 +187,8 @@ CREATE TABLE `orders` (
 INSERT INTO `orders` (`OID`, `CID`, `TotalPrice`, `Status`) VALUES
 (19, 5, 10000, 'Active'),
 (20, 5, 1000, 'Active'),
-(21, 5, 2000, 'Active');
+(21, 5, 2000, 'Active'),
+(22, 7, 1000, 'Active');
 
 -- --------------------------------------------------------
 
@@ -217,7 +230,8 @@ CREATE TABLE `seller` (
 
 INSERT INTO `seller` (`SID`, `FullName`, `NIC`, `Email`, `Password`, `Phonenumber`, `Business_name`, `B_Email`, `Bank`, `Branch`, `AccountNo`) VALUES
 (6, 'Senarath Samarasinghe', '602375896v', 'seller@gmail.com', 'Password#123', 123456789, 'Senarath Electronics', 'info@senarathe.com', 'Sampath Bank', 'Kandy', 2147483647),
-(7, 'Rohan Jayapala', '200102020203', 'rohan@gmail.com', 'Password#123', 123456789, 'Rohan jewellery', 'test@test.com', 'Sampath', 'Colombo', 2147483647);
+(7, 'Rohan Jayapala', '200102020203', 'rohan@gmail.com', 'Password#123', 123456789, 'Rohan jewellery', 'test@test.com', 'Sampath', 'Colombo', 2147483647),
+(8, 'hirosha samarasekara', '200029400286', 'hirosha@gmail.com', '#Hiro2000#', 2147483647, 'panama enterprises', 'panama@gmail.com', 'sampath', 'kandy', 123123123);
 
 -- --------------------------------------------------------
 
@@ -244,7 +258,9 @@ INSERT INTO `staff` (`Staff_ID`, `User_name`, `NIC`, `Email`, `Phone_number`, `P
 (8, 'Pamuditha Kekulandara', '200020202020', 'deliveryh@buyme.com', 710123456, 'Password#delivery', 'DeliveryM'),
 (9, 'Ajith Rathnayake', '200103030303', 'paymenth@gmail.com', 810123456, 'Password#payment', 'PaymentH'),
 (10, 'Sunil Samarakoon', '697598464v', 'deliveryp@buyme.com', 910123456, 'Password#person', 'DeliveryP'),
-(12, 'Sample  Staff', '200102201108', 'sample@gmail.com', 123456789, 'toor', 'Admin');
+(12, 'Sample  Staff1', '200102201108', 'sample@gmail.com', 123456789, 'Password#sample', 'Admin'),
+(13, 'Samaple Staff', '20001790285', 'member@gmail.com', 773969049, 'Password#123', 'Admin'),
+(15, 'Sanath Amarakoon', '697598464v', 'deliveryp1@buyme.com', 910123456, 'Password#person', 'DeliveryP');
 
 --
 -- Indexes for dumped tables
@@ -325,32 +341,32 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `CID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `CID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `deliveryperson`
 --
 ALTER TABLE `deliveryperson`
-  MODIFY `DP_ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `DP_ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `FID` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `FID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `Item_number` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Item_number` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `orderitem`
 --
 ALTER TABLE `orderitem`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `OID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `OID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `payment`
 --
@@ -360,12 +376,12 @@ ALTER TABLE `payment`
 -- AUTO_INCREMENT for table `seller`
 --
 ALTER TABLE `seller`
-  MODIFY `SID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `SID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `Staff_ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Staff_ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- Constraints for dumped tables
 --
